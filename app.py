4ad99ad1d18ab8e089012d02ee4fdbe596fb0bcc
ZyAjahT <zyajah@gmail.com>
2025-02-20 01:49:20 -0500

    Added requests package and added error handling for home page

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template
+import requests
+import logging
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
+    try:
+        URL = "https://dogapi.dog/api-docs/v2/swagger.json"
+        response = requests.get(url = URL)
+        if(response.status_code != 200):
+            raise ValueError(f"Error connecting to Dog API. Check back later.")
+        return render_template("index.html", data = response.json())
+    except ValueError as e:
+        logging.error(f"ValueError with home: {e}")
+        return render_template("errorPage.html", errorMessage = e)
+    
 if __name__=="__main__":
     app.run(debug=True)
